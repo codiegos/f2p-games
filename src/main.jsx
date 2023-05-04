bc5c59629662a541a6bbec2ba0060769abef54ca
Diego Rivera <diegosebsk@gmail.com>
2023-05-04 14:13:44 -0400

    add filter provider to main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import FiltersProvider from './context/filters'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <FiltersProvider>
+    <App />
+  </FiltersProvider>
 )
