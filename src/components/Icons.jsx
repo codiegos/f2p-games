8bc0c25854cab99e119ec173ab6758b75930666b
Diego Rivera <diegosebsk@gmail.com>
2023-05-04 14:12:54 -0400

    add MultiCheckbox to header

---
+++ src/components/Icons.jsx
@@ -0,0 +1,8 @@
+
+export function SelectDownIcon () {
+  return (
+    <svg className='w-4 h-4 ml-3' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+      <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M19 9l-7 7-7-7' />
+    </svg>
+  )
+}
--- src/components/MultiCheck.jsx
+++ src/components/MultiCheck.jsx
@@ -1,27 +1,48 @@
-import { useId } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { SelectDownIcon } from './Icons'
+import { useFilters } from '../hooks/filters'
 
-function MultiCheck ({ genreTags }) {
-  const multiCheckId = useId()
+function MultiCheck ({ genreTags, onChange }) {
+  const { filters } = useFilters()
+  const [isOpen, setIsOpen] = useState(false)
+  const dropdownRef = useRef(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [dropdownRef])
+
   return (
-    <div>
-      <label className='bg-secondary focus:ring-1 outline-none focus:ring-primary rounded-md p-1.5 peer flex items-center' htmlFor={multiCheckId}>
-        Tags
-        <svg className='w-4 h-4 ml-3' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M19 9l-7 7-7-7' />
-        </svg>
-      </label>
-      <input type='checkbox' id={multiCheckId} className='peer' hidden />
-      <div className='absolute bg-secondary rounded-md peer-checked:block' id='id1' hidden>
-        <ul className='p-2.5 space-y-2 rounded-md overflow-y-auto max-h-96'>
-          {genreTags.map((genre, i) => (
-            <li className='flex items-center rounded hover:bg-blue-300 hover:text-gray-700' key={genre}>
-              <input id={i} type='checkbox' className='w-4 h-4 mx-1' />
-              <label htmlFor={i} className='w-full p-1'>{genre}</label>
+    <div ref={dropdownRef}>
+      <button onClick={() => setIsOpen(!isOpen)} className='bg-secondary focus:ring-1 outline-none focus:ring-primary rounded-md p-2 peer flex items-center'>
+        GAME TAGS
+        <SelectDownIcon />
+      </button>
+      {isOpen && (
+        <ul className='absolute bg-secondary ring-1 ring-primary ring-inset p-2.5 space-y-2 rounded-md overflow-y-auto max-h-96 uppercase'>
+          {genreTags.map((tag, i) => (
+            <li className='flex items-center rounded hover:bg-blue-300 hover:text-gray-700' key={tag}>
+              <input
+                id={i}
+                type='checkbox'
+                checked={filters.tags.includes(tag)}
+                value={tag}
+                className='w-4 h-4 mx-1'
+                onChange={onChange}
+              />
+              <label htmlFor={i} className='w-full p-1'>{tag}</label>
             </li>
           ))}
-          <button className='bg-red-500 '>hola</button>
         </ul>
-      </div>
+      )}
     </div>
   )
 }
